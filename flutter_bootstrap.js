7817a03b2bb84b7fe56e925b3165ded53dd94660
jp2612 <jp26122411@gmail.com>
2024-12-03 22:51:30 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3554476250"
+    serviceWorkerVersion: "2821535623"
   }
 });
